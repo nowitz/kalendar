da05cf1038bc3dcd1b16bd767dd8348d0bcf867e
Jan Novak <jan.novak@datalite.cz>
2016-02-16 23:36:14 +0100

    obrazky, zpravy, css

---
+++ www/js/ErrorFactory.js
@@ -0,0 +1,27 @@
+'user strict';
+angular.module('app')
+    .factory('ErrorFactory', function () {
+
+        var errors = [
+            {id: 0, message: "Zlobíš zlato! :-D"},
+            {id: 1, message: "Ty nevíš co je za den? :-P"},
+            {id: 2, message: "Ale no tak, nepodváděj! :-("},
+            {id: 3, message: "Copak nevíš, že se nemá předbíhat? :-|"},
+            {id: 4, message: ""},
+            {id: 5, message: ""},
+            {id: 6, message: ""},
+            {id: 7, message: ""},
+            {id: 8, message: ""},
+            {id: 9, message: ""}
+        ];
+
+        return {
+            all: function () {
+                return errors;
+            },
+            get: function (pos) {
+                return errors[pos].message;
+            }
+        }
+
+    });
--- www/js/IndexController.js
+++ www/js/IndexController.js
@@ -1,6 +1,6 @@
 'user strict';
 angular.module('app')
-    .controller('IndexController', function ($scope, $rootScope, ModalService) {
+    .controller('IndexController', function ($scope, $rootScope, ModalService, $ionicLoading, ErrorFactory) {
 
         $scope.window = $rootScope.windows;
 
@@ -9,20 +9,27 @@ angular.module('app')
             var systemTime = new Date().toDateString().split(" ");
             var myTime =  $scope.window[id].time.split(" ");
 
+
+            /*
             //FIXME odremovat podminku a smazat kod
             $("#"+id).flip(true);
             $scope.window[id].status = false;
             localStorage.setItem('window', JSON.stringify($scope.window));
             setTimeout(function(){ ModalService.showMessage(id);}, 1500);
-            /*
+             */
+
             if(systemTime[1] === myTime[0] && systemTime[2] === myTime[1] && systemTime[3] === myTime[2]){
                 $("#"+id).flip(true);
                 $scope.window[id].status = false;
                 localStorage.setItem('window', JSON.stringify($scope.window));
                 setTimeout(function(){ ModalService.showMessage(id);}, 1500);
             }else{
-                setTimeout(function(){ alert("podvadis");}, 800);
-            }*/
+                var x = Math.floor((Math.random() * 3) + 1);
+                $ionicLoading.show({
+                    template: ErrorFactory.get(x),
+                    duration: 3500
+                });
+            }
         }
 
 
